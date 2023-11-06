94d0826df207a8091fcf936b2b70dd06510b217e
@andreas154 <andreas@genflix.co.id>
2023-11-06 09:25:48 +0700

    feat(android): splashscreen

---
+++ app/src/main/java/com/example/registrasi/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.registrasi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+    }
+}
